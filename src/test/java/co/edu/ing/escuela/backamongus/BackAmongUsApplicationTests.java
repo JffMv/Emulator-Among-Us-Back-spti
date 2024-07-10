7f8ec7cc06be05cb3c9edc102ef71c11cd7180c9
Jefferson Mesa <mesajefer2@gmail.com>
2024-07-10 14:45:40 -0500

    save until now

---
+++ src/test/java/co/edu/ing/escuela/backamongus/BackAmongUsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ing.escuela.backamongus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackAmongUsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
